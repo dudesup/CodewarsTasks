5eae23ae54451a59fc0e342e7bd370845c302e97
Daria Supińska <dariasupinska@gmail.com>
2020-11-16 23:26:32 +0100

    SplitString task

---
+++ src/main/java/SplitStrings.java
@@ -0,0 +1,18 @@
+/*
+Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+Examples:
+StringSplit.solution("abc") // should return {"ab", "c_"}
+StringSplit.solution("abcdef") // should return {"ab", "cd", "ef"}
+*/
+
+import java.util.Arrays;
+
+public class SplitStrings {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution("abbcdhals")));
+    }
+    public static String[] solution(String s) {
+        s = (s.length() % 2 == 0) ? s : s + "_";
+        return s.split("(?<=\\G.{2})");
+    }
+}
